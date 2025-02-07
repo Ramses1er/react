512f2c26ffa7f733aba9a102772cdde219606d4d
Ramses1er <cbk.exauce@gmail.com>
2025-02-07 17:38:17 +0100

    third commit

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,5 @@
 import { createRoot } from "react-dom/client";
 
-import App02 from "./02/App02.tsx";
+import App03 from "./03/App03.tsx";
 
-createRoot(document.getElementById("root")!).render(<App02/>);
+createRoot(document.getElementById("root")!).render(<App03/>);
